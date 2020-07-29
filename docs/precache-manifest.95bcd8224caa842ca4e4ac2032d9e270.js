130a0d3fb20e94ad560a4cfa9c8111d071b8ff79
samuelematias <hi@samuelematias.com>
2020-07-29 18:31:50 -0300

    feat(build-web): builded web on docs folder

---
+++ docs/precache-manifest.95bcd8224caa842ca4e4ac2032d9e270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59787f717b12117c22aec9afe018d4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e719c99ba9615c98d8d5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be6463490a4b21a9a575",
+    "url": "/static/js/2.aaaddcbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aaaddcbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e719c99ba9615c98d8d5",
+    "url": "/static/js/main.62d26401.chunk.js"
+  },
+  {
+    "revision": "39c80241b7dcff93d76c",
+    "url": "/static/js/runtime-main.97aa92fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
