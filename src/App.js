9e19ec58fb007d50a6b45f3169aed83a81cb3301
samuelematias <hi@samuelematias.com>
2020-07-29 18:30:07 -0300

    feat(iframe): add iframe with flutter web app link

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<div
+					className="content"
+					dangerouslySetInnerHTML={{
+						__html: `<html>
+  <iframe src="https://samuelematias.github.io/flutter_web_splash_loading/"><iframe>
+</html>`,
+					}}
+				></div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
